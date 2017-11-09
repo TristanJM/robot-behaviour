ad26cf8ebb64daa1d6d876e44258c07514e4ae1e
Tristan Murfitt <tristanjm96@gmail.com>
2017-11-09 09:54:48 +0000

    Added function structure

--- program/demo/cwk_goal_seek.c
+++ program/demo/cwk_goal_seek.c
@@ -3,16 +3,31 @@
 #include "stdlib.h"
 #include "string.h"
 #include "math.h"
-#include "../../../library/motor_led/e_init_port.h"
-#include "../../../library/motor_led/e_motors.h"
-#include "../../../library/motor_led/e_led.h"
-#include "../../../library/a_d/e_prox.h"
+#include "../../library/motor_led/e_init_port.h"
+#include "../../library/motor_led/e_motors.h"
+#include "../../library/motor_led/e_led.h"
+#include "../../library/a_d/e_prox.h"
+
+#define PI 3.14159265358979
 
 void run_goal_seek(){
 
 	/* Initialise the library stuff */
 	e_init_port();
 	e_init_motors();
-	e_init_prox();
+	/* e_init_prox(); */
 
+	while(1) {
+		/* get odometry of robot */
+
+		go_to_pos(0.1,0.1,PI/4);
+	}
+
+}
+
+void go_to_pos(float x, float y, float theta){
+
+	printf("Going to (%f, %f, %f)\n",x,y,theta);
+
+	
 }
--- program/demo/cwk_goal_seek.h
+++ program/demo/cwk_goal_seek.h
@@ -1,1 +1,2 @@
-void run_goal_seek();	// Find coordinate
+void run_goal_seek(void);	// Find coordinate
+void go_to_pos(float x, float y, float theta);
