e7df63670e7fde07d6bd275d3b5a42c1f86daaa3
Tristan Murfitt <tristanjm96@gmail.com>
2017-11-09 09:28:34 +0000

    Init branch commit

--- program/demo/cwk_goal_seek.c
+++ program/demo/cwk_goal_seek.c
@@ -2,3 +2,17 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "math.h"
+#include "../../../library/motor_led/e_init_port.h"
+#include "../../../library/motor_led/e_motors.h"
+#include "../../../library/motor_led/e_led.h"
+#include "../../../library/a_d/e_prox.h"
+
+void run_goal_seek(){
+
+	/* Initialise the library stuff */
+	e_init_port();
+	e_init_motors();
+	e_init_prox();
+
+}
---
+++ program/demo/cwk_goal_seek.h
@@ -0,0 +1,1 @@
+void run_goal_seek();	// Find coordinate
