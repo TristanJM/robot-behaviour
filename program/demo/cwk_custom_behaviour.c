2b7d5d252da7511d5afae0a99fa03353ba88a5e8
Brandon Randle <branr62@gmail.com>
2017-11-23 09:32:00 +0000

    Really basic skeleton

--- program/demo/cwk_custom_behaviour.c
+++ program/demo/cwk_custom_behaviour.c
@@ -2,3 +2,31 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "math.h"
+#include "../../../library/motor_led/e_init_port.h"
+#include "../../../library/motor_led/advance_one_timer/e_motors.h"
+#include "../../../library/camera/fast_2_timer/e_poxxxx.h"
+#include "../../../library/motor_led/advance_one_timer/e_led.h"
+
+
+
+void do_goal_seek(){
+
+	
+	
+	
+
+}
+
+
+int get_red_level(){
+
+}
+
+int get_green_level(){
+
+}
+
+int get_blue_level(){
+
+}
