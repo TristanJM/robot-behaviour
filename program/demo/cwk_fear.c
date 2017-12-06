06194ce2a5f8332bb6818e966d8389bafd5c69d5
Brandon Randle <branr62@gmail.com>
2017-12-06 17:05:05 +0000

    Simplify in the hope that it runs

--- program/demo/cwk_fear.c
+++ program/demo/cwk_fear.c
@@ -10,6 +10,10 @@
 #include "a_d/e_prox.h"
 #include "utility.h"
 
+/* Settings */
+#define MOTOR_INTENSITY 100;  //Increase or decrease this to make the robot move & turn quicker.
+#define WAIT_TIME 1000;       //Decrease this to update quicker / move smaller amounts between checks.
+
 void run_fear(){
 
 	int i;                              // Used in loops.
@@ -19,32 +23,26 @@ void run_fear(){
 	int randomNumber;                   // Will hold a random number at one point.
 	char buffer[80];					// For uart debug
 
-	/* Settings */
-	int motor_intensity = 100;  //Increase or decrease this to make the robot move & turn quicker.
-	int wait_time = 1000;       //Decrease this to update quicker / move smaller amounts between checks.
+	
 
 	/* Initialise the library stuff */
-	e_init_port();
 	e_init_port();
 	e_init_uart1();
 	e_init_motors();
 	//e_calibrate_ir();
 
 	sprintf(buffer, "Starting run_fear\r\n");
+	memset(&buffer[0], 0, sizeof(buffer));
 	e_send_uart1_char(buffer, strlen(buffer));
 
 
     // vvv  For debug only   vvv
 	e_set_speed_left(100);
 	sprintf(buffer, "Left wheel 100\r\n");
+	memset(&buffer[0], 0, sizeof(buffer));
+	e_send_uart1_char(buffer, strlen(buffer));
+	// ^^^   For debug only   ^^^
 
-
-
-
-	/* Set up a rng, seed it with the time */
-	//srand(time(NULL));
-
-	
 
 	while(1)
 	{
@@ -56,6 +54,8 @@ void run_fear(){
 			if(prox[i] > 1000){
 				e_set_led(i, 1);
 				sprintf(buffer, "Prox $i = $i\r\n", i, prox[i] );
+				memset(&buffer[0], 0, sizeof(buffer));
+				e_send_uart1_char(buffer, strlen(buffer));
 			} else {
 				e_set_led(i, 0);
 			}
@@ -63,18 +63,11 @@ void run_fear(){
 
 
 
-		/* Set our wheel speeds to something random and small
-		 * This is what our robot does when nothing is about.
-		 * Makes sense to move slightly here in case there's
-		 * an object *between* the proximity sensors.
-		 * Whatever we set here will be overridden by the next
-		 * block if there's actually something on the sensors.
-		 * tl;dr: default to a random number
+		/* Set our wheel speeds to 0 to start with.
+		 * If there's anything in the proximity sensors though, this will be overridden
 		 */
-		 //randomNumber = (rand() % 20) - 10; //Get a random number between -10 and 10
-		 randomNumber = 5; //Chose this randomly
-		 leftwheel =    randomNumber;       // Set left wheel to whatever we just got
-		 rightwheel =  -randomNumber;       //Set right wheel to opposite of whatever left is
+		leftwheel =   0;       // Set left wheel to whatever we just got
+		rightwheel =  0;       //Set right wheel to opposite of whatever left is
 
 
 
@@ -87,50 +80,50 @@ void run_fear(){
 
 		 // Right side of robot
 		if(prox[0] > 1000){
-			leftwheel =		-3 * motor_intensity;
-			rightwheel =	 3 * motor_intensity;
+			leftwheel =		-3 * MOTOR_INTENSITY;
+			rightwheel =	 3 * MOTOR_INTENSITY;
 		}
 		if(prox[1] > 1000){
-			leftwheel =		-2 * motor_intensity;
-			rightwheel =	 2 * motor_intensity;
+			leftwheel =		-2 * MOTOR_INTENSITY;
+			rightwheel =	 2 * MOTOR_INTENSITY;
 		}
 		if(prox[2] > 1000){
-			leftwheel =		-1 * motor_intensity;
-			rightwheel =	 1 * motor_intensity;
+			leftwheel =		-1 * MOTOR_INTENSITY;
+			rightwheel =	 1 * MOTOR_INTENSITY;
 		}
 
 		// Left side of robot
 		if(prox[7] > 1000){
-			leftwheel =		 3 * motor_intensity;
-			rightwheel =	-3 * motor_intensity;
+			leftwheel =		 3 * MOTOR_INTENSITY;
+			rightwheel =	-3 * MOTOR_INTENSITY;
 		}
 		if(prox[6] > 1000){
-			leftwheel =		 2 * motor_intensity;
-			rightwheel =	-2 * motor_intensity;
+			leftwheel =		 2 * MOTOR_INTENSITY;
+			rightwheel =	-2 * MOTOR_INTENSITY;
 		}
 		if(prox[5] > 1000){
-			leftwheel =		 1 * motor_intensity;
-			rightwheel =	-1 * motor_intensity;
+			leftwheel =		 1 * MOTOR_INTENSITY;
+			rightwheel =	-1 * MOTOR_INTENSITY;
 		}
 
 
 		/* Now for the rear sensors,
 		 * if we see something on these sensors, run forward */
 		if(prox[4] > 1000){
-			leftwheel =		4 * motor_intensity;
-			rightwheel =	4 * motor_intensity;
+			leftwheel =		4 * MOTOR_INTENSITY;
+			rightwheel =	4 * MOTOR_INTENSITY;
 		}
 
 		if(prox[3] > 1000){
-			leftwheel =		4 * motor_intensity;
-			rightwheel =	4 * motor_intensity;
+			leftwheel =		4 * MOTOR_INTENSITY;
+			rightwheel =	4 * MOTOR_INTENSITY;
 		}
 
 
 		/* Now actually set the motor speeds */
 		e_set_speed_left(leftwheel);
 		e_set_speed_right(rightwheel);
-		wait(wait_time);
+		wait(WAIT_TIME);
 
 
 	}
