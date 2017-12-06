00a9d7f1e8f14572f159f144ca4946475e80ead3
Brandon Randle <branr62@gmail.com>
2017-12-06 14:02:15 +0000

    Header file for traffic light behaviour

---
+++ program/demo/cwk_custom_behaviour.h
@@ -0,0 +1,14 @@
+
+void do_goal_seek();
+
+void update_levels();
+
+void update_proximity(int *arr);
+
+int is_obstacle_ahead(int *arr);
+
+int get_dominant_rgb(int r, int g, int b);
+
+void start_motors();
+
+void stop_moving();
