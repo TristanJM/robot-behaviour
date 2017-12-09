78c93c8fcfcd6e209b690962ceb14ea7fdbf1843
Brandon Randle <branr62@gmail.com>
2017-12-09 20:57:14 +0000

    Follow walls
    Right now the camera confuses things a little - since the way the
    camera is set up means it ALWAYS sees red, green or blue - it’s never
    neutral. But, it should follow a corridor, and if one wall disappears
    then it hugs the remaining one. If it’s meant to be turning next then
    it’ll hug the one that disappeared instead

--- program/demo/cwk_custom_behaviour.c
+++ program/demo/cwk_custom_behaviour.c
@@ -11,6 +11,8 @@
 #include "../../library/a_d/advance_ad_scan/e_ad_conv.h"
 #include "../../library/a_d/advance_ad_scan/e_prox.h"
 #include "../../library/camera/fast_2_timer/e_poxxxx.h"
+#include "a_d/advance_ad_scan/e_ad_conv.h"
+#include "a_d/advance_ad_scan/e_prox.h"
 
 #include "utility.h"
 #include "runwallfollow.h"
@@ -19,6 +21,18 @@
 
 #define LEFT_FOLLOW			0		// behaviors IDs	
 #define RIGHT_FOLLOW		1 
+
+// State machine states
+#define STOP_MOVING           0     // Just don't move at all
+#define POWER_THROUGH         1     // Move forward, ignoring front proximity sensor
+#define TURN_NEXT             2     // Move forward, when one prox sensor drops off, transition into FOLLOW_[missing]_WALL
+#define FOLLOW_RIGHT_WALL     3     // Hug the right wall, until the left prox sensor comes back
+#define FOLLOW_LEFT_WALL      4     // Hug the left wall, until the right prox sensor comes back
+#define FOLLOW_BOTH_WALLS     5     // Move forward keeping distance from both walls. If one prox sensor drops off, transition into FOLLOW_[remaining]_WALL
+#define TURN_AROUND           6     // Rotate until the sensors equalise and then transition to TURN_NEXT
+
+#define TURN_AGGRESSION       1     // Changes how quickly the robot turns to get back on track when wall following
+#define WALL_HUG_DISTANCE   250     // How close to hug a wall when following one of them
 
 #define BIAS_SPEED      	350		// robot bias speed
 #define SENSOR_THRESHOLD	300		// discount sensor noise below threshold
@@ -44,24 +58,34 @@ void run_custom() {
     int distances[8]; // array keeping the distance sensor readings
     int i;
     int primary_colour;
-    int waiting_for_turn = 0;
     int leftwheel;
     int rightwheel;
+    int state;
+    int sensor_difference;
 
     e_init_port();
-    e_init_motors();
     e_init_uart1();
-
+    e_init_ad_scan(ALL_ADC);
+	e_init_motors();
+	e_start_agendas_processing();
+	e_calibrate_ir();
+    
     // camera set up
     e_poxxxx_init_cam();
     e_poxxxx_config_cam(0, (ARRAY_HEIGHT - 4) / 2, 640, 4, 8, 4, RGB_565_MODE);
     e_poxxxx_write_cam_registers();
 
-    e_start_agendas_processing();
+    
+    
 
-    e_calibrate_ir();
+    
+    // Start by following both walls.
+    state = FOLLOW_BOTH_WALLS;
 
+    
     while (1) {
+               
+        // Clear LEDs
         e_led_clear();
         
         // Get levels from camera
@@ -70,31 +94,125 @@ void run_custom() {
         // Get dominant colour
         primary_colour = get_dominant_rgb(red_level, green_level, blue_level);
         
+        
         // React to colour
         if (primary_colour == 1) {
             // RED DETECTED
             e_set_led(4, 1);    
-            leftwheel = 0;
-            rightwheel = 0;
+            state = STOP_MOVING;
         } else if (primary_colour == 2) {
             // GREEN DETECTED
-            leftwheel = BIAS_SPEED;
-            rightwheel = BIAS_SPEED;
+            state = POWER_THROUGH;
         } else if (primary_colour == 3) {
             // BLUE DETECTED
-            rotate_robot(180);
-            waiting_for_turn = 1;
-            leftwheel = BIAS_SPEED;
-            rightwheel = BIAS_SPEED;
+            state = TURN_AROUND;
         } else {
             // NO COLOUR (DEBUG ONLY)
         }
 
+        leftwheel = 0;
+        rightwheel = 0;
         
-        // Stay within walls of road
+
         
-        // Read sensor values
-        getSensorValues(distances);
+        /* Update Proximity Sensor value array, also turn on LEDs if detected */
+		for(i=0; i<8; i++){
+			distances[i] = e_get_prox(i);
+			if(distances[i] > 250){
+				e_set_led(i, 1);
+			} else {
+				e_set_led(i, 0);
+			}
+		}
+        
+        
+        
+        if( state == FOLLOW_BOTH_WALLS || state == TURN_NEXT ){
+            // If we're supposed to be following both walls, make sure IR6 and IR1 are similar
+            leftwheel = MAXSPEED;
+            rightwheel = MAXSPEED;
+            
+            // - difference means facing too far right, + too far left 
+            sensor_difference = distances[6] - distances[1];
+            
+            // Adjust wheel speeds to correct for difference in sensor value
+            rightwheel -= sensor_difference * TURN_AGGRESSION;
+            leftwheel  += sensor_difference * TURN_AGGRESSION;
+            
+            // Check whether the L or R wall hasn't literally just dropped away. If it has:
+            // If we're meant to be going straight, hug the right wall
+            // If we're meant to be turning, hug the one that disappeared
+            if(distances[6] < 150){
+                if(state == FOLLOW_BOTH_WALLS) state = FOLLOW_RIGHT_WALL;
+                if(state == TURN_NEXT) state = FOLLOW_LEFT_WALL;
+            }
+            
+            if(distances[1] < 150){
+                if(state == FOLLOW_BOTH_WALLS) state = FOLLOW_LEFT_WALL;
+                if(state == TURN_NEXT) state = FOLLOW_RIGHT_WALL;
+            }
+            
+            
+        }
+        
+        
+        if(state == FOLLOW_LEFT_WALL){
+            // If we're following the left wall, make sure IR6 and IR4 are similar and that IR4 is below WALL_HUG_DISTANCE
+            
+            
+            rightwheel = MAXSPEED;
+            leftwheel = MAXSPEED;
+            
+            // Make sure it's pointing roughly toward the left wall (eg. front closer than rear)
+            if(distances[6] > distances[4]){
+                rightwheel += sensor_difference * TURN_AGGRESSION;
+            }
+            
+            // Don't steer left if we're near the left wall
+            if(distances[6] > WALL_HUG_DISTANCE){
+                leftwheel = 0;
+                rightwheel = -MAXSPEED;
+            }
+            
+            // If the right sensor sees a wall again, go back to regular corridor-style mode 
+            if(distances[1] > 150){
+                state = FOLLOW_BOTH_WALLS;
+            }
+            
+        }
+        
+        
+        
+        if(state == FOLLOW_RIGHT_WALL){
+            // If we're following the left wall, make sure IR1 and IR3 are similar and that IR4 is below WALL_HUG_DISTANCE
+            
+            
+            rightwheel = MAXSPEED;
+            leftwheel = MAXSPEED;
+            
+            // Make sure it's pointing roughly toward the right wall (eg. front is closer than rear)
+            if(distances[1] > distances[3]){
+                leftwheel += sensor_difference * TURN_AGGRESSION;
+            }
+            
+            // Don't steer right if we're near the left wall
+            if(distances[1] > WALL_HUG_DISTANCE){
+                leftwheel = -MAXSPEED;
+                rightwheel = 0;
+            }
+            
+            // If the left sensor sees a wall again, go back to regular corridor-style mode 
+            if(distances[3] > 150){
+                state = FOLLOW_BOTH_WALLS;
+            }
+            
+        }
+        
+        
+
+        
+        
+        
         
         // TODO:
         // Check "distances" for left and right sensor
