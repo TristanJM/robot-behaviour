144346bc6ac49bb9c116437ac321b55112d4a359
Tino Aranjani <t.b.aranjani-14@student.lboro.ac.uk>
2017-12-10 05:58:51 +0000

    Trying something new
    Error: 'e_start_led_blinking' undeclared (first use in this function),
    can' seem to over come this error :(

--- program/demo/cwk_love.c
+++ program/demo/cwk_love.c
@@ -3,115 +3,111 @@
 #include "stdlib.h"
 #include "string.h"
 #include "math.h"
-#include "../../library/motor_led/e_epuck_ports.h"
-#include "../../library/motor_led/e_init_port.h"
-#include "../../library/motor_led/advance_one_timer/e_motors.h"
-#include "../../library/motor_led/advance_one_timer/e_agenda.h"
-#include "../../library/a_d/advance_ad_scan/e_prox.h"
-#include "../../library/a_d/advance_ad_scan/e_ad_conv.h"
-#include "../../library/motor_led/advance_one_timer/e_led.h"
+#include "uart/e_uart_char.h"
+#include "motor_led/e_epuck_ports.h"
+#include "motor_led/e_init_port.h"
+#include "motor_led/e_led.h"
+#include "motor_led/advance_one_timer/e_motors.h"
+#include "motor_led/advance_one_timer/e_agenda.h"
+#include "motor_led/advance_one_timer/e_led.h"	
+#include "a_d/advance_ad_scan/e_prox.h"
+#include "a_d/advance_ad_scan/e_ad_conv.h"
+#include "utility.h"
+#include "cwk_love.h"
 
-//Constants
-#define NUM_SENSORS 8			// number of sensors 
-#define WAIT_TIME 30000			// interval time between each iteration of the main loop
-#define HEART_BEAT_TIME 800000	// max interval time before a heartbeat
+/* 	Love - Once activated all 8 LEDs will start 
+	blinking in a motion to represent a heartbeat. 
+	Once the front IR sensors has detected something it 
+	will go towards that direction and will start following 
+	it. As it gets closer and closer to the obstacle (lover) 
+	the LEDs will stop blinking to symbolise a skip of a 
+	heartbeat :P
+*/
 
+
+// fuction used to calculate the linear speed to set on 
+// each wheel depending on the information from the
+// front sensors - IR 0 & 7 
+int lin_speed_calc(int dist, int gain)
+{
+	int range = 350;
+	float x = 0.1;
+	int t = 3;
+	int linVal = 0;
+	int diffDist = range-dist;
+	int basic_speed;
+
+	linVal = linVal + x * diffDist / t;
+	basic_speed = (diffDist + linVal) * gain;
+
+	if(basic_speed >= 1000)
+	{
+		linVal = 999/gain - diffDist;
+		if(linVal > 60)	
+			linVal = 60.0;
+		basic_speed = 999;
+	}
+	else if(basic_speed <= -1000)
+	{
+		linVal = -999/gain + diffDist;
+		if(linVal < -10)	
+			linVal = -10.0;
+		basic_speed = -999;
+	}
+	return basic_speed;
+}
+
+// fuction used to calculate the angular speed to set on 
+// each wheel depending on the information from the
+// 4 front sensors IR 0,1,6 & 7 
+int angle_speed_calc(int pos, int gain)
+{
+	int range = 0;
+	int angle_speed = 0;
+	int diffDist = range - pos;
+
+	angle_speed = diffDist*gain;
+
+	if(angle_speed >= 1000)
+		angle_speed = 999;
+	else if(angle_speed <= -1000)
+		angle_speed = -999;
+
+	return angle_speed;
+}
+
+// calculating the speed to follow the lover
+void follow_lover(void)
+{
+	// skipping a heartbeat when lover is spotted/near 
+	if(e_get_calibrated_prox(0) || e_get_calibrated_prox(7) < 1000)
+		e_activate_agenda(e_start_led_blinking, 500);
+	else 
+	{
+		e_activate_agenda(e_stop_led_blinking, 250); 
+		e_activate_agenda(e_set_blinking_cycle, 25000);	// restarting the heartbeat effect after the skip
+	}
+
+	int basic_speed = lin_speed_calc((e_get_calibrated_prox(7)+e_get_calibrated_prox(0))/2, 6);
+	int angle_speed = angle_speed_calc((e_get_calibrated_prox(0)+e_get_calibrated_prox(1)) -
+										(e_get_calibrated_prox(7)+e_get_calibrated_prox(6)), 4);
+
+	// setting the wheel speed
+	e_set_speed_left (basic_speed - angle_speed);
+	e_set_speed_right(basic_speed + angle_speed);
+}
+
+
+// main method for love ehaviour 
 void run_love(void)
 {
-	long i = 0;							//loop counter
-	int leftWheel = 0;					//speed of left wheel
-	int rightWheel = 0;					//speed of right wheel
-	long heartBeatCntr = 0;				//Counts how many steps since last heartbeat
-	int heartBeatLEDs = 0;				//0 for all LEDs off, 1 for all LEDs on
-	
-	//Initialise setup libraries
 	e_init_port();
 	e_init_motors();
-	e_led_clear();
-
-	//start with body lights on and moving forwards
-	e_set_body_led(1);
-	e_set_steps_right(1);
-	e_set_steps_left(1);
-	e_set_speed_right(100);
-	e_set_speed_left(100);
+	e_init_ad_scan(ALL_ADC);
 	
-	while (1) {
-		// starting off
-		leftWheel = leftWheel * 0.9;
-		rightWheel = rightWheel * 0.9;
-		leftWheel+=38;
-		rightWheel+=38;
-		
-		//adjusts the speed if something is near it in that specific direction
-		for (i=0;i<NUM_SENSORS;i++) {
-			//roughly 1000 means its about to touch
-			//any less than 500 and it starts having false positives
-			if (e_get_prox(i)>=500) {	
-				switch (i) {
-					//left side sensors
-					case 0:leftWheel+=300;rightWheel+=100;break;
-					case 1:leftWheel+=100;rightWheel-=100;break;
-					case 2:leftWheel+=300;rightWheel-=400;break;
-					//back sensors
-					case 3:leftWheel+=400;rightWheel-=300;break;
-					case 4:leftWheel+=400;rightWheel-=300;break;
-					//right side sensors
-					case 5:leftWheel-=400;rightWheel+=300;break;
-					case 6:leftWheel-=100;rightWheel+=100;break;
-					case 7:leftWheel+=300;rightWheel+=100;break;
-				}
-			}
-		}	
-		
-		//performing heartBeat
-		heartBeatCntr++;
-		//get mean average of both front sensors
-		int frontProx = (e_get_prox(0) + e_get_prox(7)) / 2;
-		if (frontProx < 1) frontProx = 1;
-		//calculates maximum time for heartbeat
-		long heartBeatMax = HEART_BEAT_TIME;
+	e_calibrate_ir();
 
-		if (e_get_prox(0)>300 || e_get_prox(7)>300)
-			heartBeatMax = HEART_BEAT_TIME/2;
-		if (e_get_prox(0)>900 || e_get_prox(7)>900)
-			heartBeatMax = HEART_BEAT_TIME/4;
-		
-		//measure as to when to do a heartbeat
-		if (heartBeatCntr*WAIT_TIME > heartBeatMax) {
-			//toggle between 0 and 1
-			heartBeatLEDs = 1 - heartBeatLEDs;
-			//set LEDs
-			for (i=0;i<NUM_SENSORS;i++) {
-				e_set_led(i,heartBeatLEDs);
-			}
-			//reset timer
-			heartBeatCntr = 0;
-		}
-		
-		//action for front sensors
-		if (e_get_prox(0)>1000 || e_get_prox(1)>1000) {
-			if (e_get_prox(0)> e_get_prox(7)) {
-				leftWheel  = 200;
-				rightWheel = -200;
-			}
-			else if (e_get_prox(7)>1900) {
-				leftWheel  = -200;
-				rightWheel = 200;
-			}
-			for(i=0;i<80000;i++) {asm("nop");}
-		}
-		
-	
-		//limit max speed (to 800)
-		if (leftWheel>800) { leftWheel = 800; }
-		if (rightWheel>800) { rightWheel = 800; }
-		if (leftWheel<-800) { leftWheel = -800; }
-		if (rightWheel<-800) { rightWheel = -800; }
-		
-		//wait time
-		for(i=0;i<WAIT_TIME;i++) {asm("nop");}
-		e_set_steps_right(rightWheel);
-		e_set_steps_left(leftWheel);
-	}
+	e_activate_agenda(follow_lover, 650);
+	e_start_agendas_processing();
+	while(1);
 }
