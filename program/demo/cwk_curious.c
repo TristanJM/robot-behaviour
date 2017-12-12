868959304e8f7138e2bacdb1b895a023d12ceadf
Sarthak <shadoweye14@users.noreply.github.com>
2017-12-12 15:12:27 +0000

    Cleaned up code, added agendas, added fix or LED timers and prox sensors

--- program/demo/cwk_curious.c
+++ program/demo/cwk_curious.c
@@ -22,25 +22,12 @@
 #define MOTOR_BIAS_SPEED    200     // <= 500 [lt/e 500]
 #define MOTOR_MAX_SPEED     500     // <= 500 [lt/e 500]
 
-void animate_lights(int flag, int wait_time) {
-               long k;
-               switch (flag) {
-        case 0:
-            e_set_led(8, 1);
-            wait(wait_time);
-            e_set_led(8, 0);
-            break;
-        case 1:
-
-            for(k=0;k<50;k++){
-            k2000_led();            
-            wait(wait_time);
-            }
-            break;
-        default:
-            break;
-    }
-}
+#define CONF_CURIOUS_MODE         0             // Uses Prox sensors if set to 1.
+#define CONF_WAIT_TIME            50000         // Wait time for all operations
+#define CONF_TURN_WAIT            500           // Time needed to complete the turn
+#define CONF_ANIMATELED_WAIT      10000         // Wait time for LED Animations /// 1 Second
+#define CONF_CAL_IR_ACTIVATION    100           // Assumed Range 0-100
+#define CONF_PROXIMITY_THRESHOLD  0.9
 
 void epuck_set_speed(int speed, int wait_time) {
     e_set_speed_left(speed);
@@ -55,7 +42,7 @@ void epuck_shake(int speed, int wait_time, int iterations) {
         wait(wait_time);
         e_set_speed(0, 1 * speed);
         wait(wait_time);
-        e_set_speed(0, 1 *speed);
+        e_set_speed(0, 1 * speed);
         wait(wait_time);
         e_set_speed(0, -1 * speed);
         wait(wait_time);
@@ -69,7 +56,7 @@ void update_sensor_array(int *state_prox) {
     }
 }
 
-void creep(int max_ir_index, int current_ir_activation, int proximity_threshold) {
+void epuck_creep(int max_ir_index, int current_ir_activation) {
     int linear_speed, angular_speed;
 
     /* Flags */
@@ -115,7 +102,7 @@ void creep(int max_ir_index, int current_ir_activation, int proximity_threshold)
             angular_speed = (int) floor((120 / 2) * (1));
             break;
         default:
-            angular_speed = (rand() % 2*120)-120;
+            angular_speed = (rand() % 2 * 120) - 120;
             break;
     }
 
@@ -128,6 +115,15 @@ void creep(int max_ir_index, int current_ir_activation, int proximity_threshold)
     e_set_speed(linear_speed, angular_speed);
 }
 
+void epuck_led_wait(int led_wait) {
+    long a;
+    for (a = 0; a < led_wait; a++) { asm("nop"); }
+}
+
+int rand_range(int min, int max) {
+    return rand() % (max + 1 - min) + min;
+}
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCDFAInspection"
 
@@ -137,38 +133,29 @@ void run_curious() {
     e_init_ad_scan(ALL_ADC);
     e_init_motors();
     e_init_uart1();
-    
-    e_start_agendas_processing();
+
     e_calibrate_ir();
+    e_start_agendas_processing();
 
     srand(time(NULL));
 
-    /* Configurations */
-    int conf_curious_mode = 1;          // Uses Prox sensors if set to 1.
-    int conf_wait_time = 50000;         // Wait time for all operations
-    int conf_turn_wait = 500;           // Time needed to complete the turn
-    int conf_animateled_wait = 50000;   // Wait time for LED Animations
-    int conf_cal_ir_activation = 200;    // Assumed Range 0-100
-    int conf_proximity_threshold = 0.9;
-    
-    long wait_iter, i, j ,k;
-
+    long i, j, k;
 
     /* State Variables */
     int state_prox[SENSORS] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    while (1) {
-        if (conf_curious_mode) {
-            // Prox Sensor Enabled Code
+    if (CONF_CURIOUS_MODE) {
+        // Prox Sensor Enabled Code
+        while (1) {
             //update_sensor_array(state_prox);
 
             for (i = 0; i < SENSORS; i++) {
                 state_prox[i] = e_get_prox(i);
             }
-            
+
             int max_ir_index = 8;
             for (i = 0; i < SENSORS; ++i) { // Update LED on IR Activation
-                if (state_prox[i] > conf_cal_ir_activation) {
+                if (state_prox[i] > CONF_CAL_IR_ACTIVATION) {
                     e_set_led(i, 1);
                     max_ir_index = i;
                 } else {
@@ -177,24 +164,28 @@ void run_curious() {
             }
 
             // Creep towards object - Creep slows down, the closer the object is.
-            creep(max_ir_index, (max_ir_index < SENSORS) ? state_prox[max_ir_index] : 0, conf_proximity_threshold);
+            epuck_creep(max_ir_index, (max_ir_index < SENSORS) ? state_prox[max_ir_index] : 0);
             // Repeat
-            for(i=0; i<50000; i++) { asm("nop"); }
+            for (i = 0; i < 50000; i++) { asm("nop"); }
+        }
 
-        } else {
+    } else {
+        while(1){
             // Basic Movement Code
-            //animate_lights(0, conf_animateled_wait);
+            e_activate_agenda(e_blink_led, CONF_ANIMATELED_WAIT * 2.5);
+            e_pause_agenda(e_blink_led);
             // Move Forward - Random
-            e_set_speed(rand()%(400+1-350)+350, rand()%(2*80)-80);
-            wait(conf_wait_time*4);
-            //wait(conf_wait_time*4);
+            e_set_speed(rand_range(350, 400), rand_range(-80, 80));
+            wait(CONF_WAIT_TIME * 4);
             // Shake
-            epuck_shake(500, conf_turn_wait, 1);
+            epuck_shake(500, CONF_TURN_WAIT, 1);
             animate_lights(1, 1000);
             // Flash Lights
-            //animate_lights(0, conf_animateled_wait);
+            e_activate_agenda(e_blink_led, CONF_ANIMATELED_WAIT * 2.5);
+            e_pause_agenda(e_blink_led);
         }
     }
 }
 
 #pragma clang diagnostic pop
+// Copyright - Very Disgruntled Students [VDS]
--- program/demo/cwk_curious.h
+++ program/demo/cwk_curious.h
@@ -1,1 +1,8 @@
 void run_curious();
+
+void epuck_set_speed(int speed, int wait_time);
+void epuck_shake(int speed, int wait_time, int iterations);
+void update_sensor_array(int *state_prox);
+void epuck_creep(int max_ir_index, int current_ir_activation);
+void epuck_led_wait(int led_wait);
+int rand_range(int min, int max);
